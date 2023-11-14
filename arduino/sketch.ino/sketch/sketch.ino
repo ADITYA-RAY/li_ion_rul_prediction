e1aec7040386b451e4da48b3afa491d3068d46a0
aditya ray <aditya90005@gmail.com>
2023-11-14 12:17:45 +0530

    firmware updated

--- arduino/sketch.ino/sketch/sketch.ino
+++ arduino/sketch.ino/sketch/sketch.ino
@@ -1,16 +1,9 @@
-#include <LiquidCrystal_I2C.h>
-#include <Wire.h>
-
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
 #define voltagePin A1
 #define currentPin A0
 #define temperaturePin A3
-#define sdaPin A4
-#define sclPin A5
 #define switchPin 12
 
-float ref_voltage = 5.0;
+float ref_voltage = 5;
 bool charging = false;
 
  
@@ -22,8 +15,6 @@ void setup()
    digitalWrite(2, HIGH);
    digitalWrite(3, HIGH);
    digitalWrite(12, HIGH);
-   lcd.init();
-   lcd.backlight();
    Serial.begin(9600);
 }
 
@@ -42,7 +33,7 @@ float computeVoltage(){
   float in_voltage = 0.0;
   int adc_value = 0;
   adc_value = analogRead(voltagePin);
-  adc_voltage  = (adc_value * ref_voltage) / 1024.0; 
+  adc_voltage  = (adc_value * 5) / 1024.0; 
   in_voltage = adc_voltage / (R2/(R1+R2)) ;
   return in_voltage;
 }
@@ -53,7 +44,7 @@ float computeCurrent(){
   float in_current = 0;
   current_adc = analogRead(currentPin);
   current_voltage  = (current_adc * ref_voltage*1000) / 1024.0; 
-  in_current = (current_voltage-2500)*1000/66.00;  
+  in_current = (current_voltage-2500)*1000/100.00;  
   return in_current;
 
 }
@@ -69,25 +60,7 @@ void switchControl(float voltage){
 }
 
 void displayData(float voltage, float current, float temperature){
-  lcd.setCursor(0, 0);
-  lcd.print("V=");
-  lcd.setCursor(2, 0);
-  lcd.print(voltage);
-  lcd.setCursor(7, 0);
-  lcd.print("C=");
-  lcd.setCursor(9, 0);
-  lcd.print((current + 100)/1000);
-  lcd.setCursor(0, 1);
-  lcd.print("T=");
-  lcd.setCursor(2, 1);
-  lcd.print(temperature);
-  lcd.setCursor(8, 1);
-  if(charging){
-    lcd.print("Charg.");
-  }
-  else{
-    lcd.print("Dischar.");
-  }
+
   Serial.print(voltage);
   Serial.print(", ");
   Serial.print((current + 100)/1000,2);
@@ -112,14 +85,14 @@ void loop(){
     delay(5);
   }
   
-  voltage = voltage / 200 + 0.04;
+  voltage = voltage / 200;
   current /= -200;
   temperature /= 200;
 
    
 
   switchControl(voltage);
-  displayData(voltage,current,temperature);
+  displayData(voltage + 0.04,current,temperature);
     
-  delay(10000);
+  delay(2000);
 }
